e591e4f839f6e9e7d49e6e1e2dfe31a9b2237719
text-edit <DanielMcKee@DM-MBP-15.local>
2018-08-26 21:18:06 -0500

    new push

--- testing.js
+++ testing.js
@@ -3,3 +3,10 @@
 // messed up creating a new branch off of master, called 'spaceBranch'
 import test from './crypto';
 test //import test 
+
+function test2(){
+    "test2"
+}
+
+test2
+export test2;
