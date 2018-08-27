f1c1a537021e478ecef39cda1a9fb19e829bc9ef
text-edit <DanielMcKee@DM-MBP-15.local>
2018-08-26 21:08:37 -0500

    new test @ 9:08

--- crypto.js
+++ crypto.js
@@ -5,3 +5,5 @@ const test = "Testing" {
 function test(){
     test:"testing";
 }
+
+export test; 
--- testing.js
+++ testing.js
@@ -1,3 +1,5 @@
 // just testing to see if it works.
 
 // messed up creating a new branch off of master, called 'spaceBranch'
+import test from './crypto';
+test //import test 
